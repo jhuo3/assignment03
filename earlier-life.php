79753bf625daf844b3dc06aeae60d1bdedd32bee
Baz D <deweybazirake@gmail.com>
2019-09-27 16:50:45 -0400

    Update earlier-life.php

--- earlier-life.php
+++ earlier-life.php
@@ -1,27 +1,25 @@
 <!doctype html>
-
-	<html land="en">
+<html land="en">
 
 	<head>
 		<meta charset="utf-8">
-		<title>Home</title>
+		<title>Team Havana | Assignment 03: IA</title>
 		<link rel="stylesheet" href ="css/styles.css">
 	</head>
 
-	<body>
+	<body class = "container">
 
 		<header>
-
-			<h1>The Persons of Interest: DMS Majors</h1>
-			<ul>
-				<li>Alex Chase</li>
-				<li>Jennifer Ngene</li>
-				<li>Maria Mancheno</li>
-			</ul>
-
+			<h1 class="homeh1" style = "font-size: 1.2em; left: -65px; font-size: 42px;" > We are DMS</h1>
 		</header>
 
-		<nav>
+		<section class="section">
+			<h2>Topics of Similarity</h2>
+			<p>The aforementioned students are all declared Digital Media Studies Majors, and all go about discussing their early lives, interests, and why it is that they are a DMS major.</p>
+		</section>
+
+		<!-- Possibility of adding aside -->
+		<nav class="menu">
 
 			<ul>
 				<li><a href="index.php">Home</a></li>
@@ -34,18 +32,76 @@
 
 		<main>
 
+			<!--  <a href="alex.php">
+				<h1 class="homeh1" style = "font-size: 1.2em; left: -65px; font-size: 42px;" > Welcome to our page!</h1>
+			</a> -->
+
+
 			<h2>Earlier Life</h2>
-				<h3>Alex Chase</h3>
-					<p>Alex Chase was evidently born at a young age (who knew??) and raised by wolves until he finally made the realization that fighting for scraps of meat amongst wolf pups was not only not the right life path for him, but was in fact gen-ed credit in high school terms! Alex graduated from high school with honors and proceeded to go to the University of Rochester, just like the rest of us.</p>
 
-				<h3>Jennifer Ngene</h3>
-					<p>Jennifer Ngene was born in The Gambia in 1999 and was raised between there and Nigeria for the first few years of life. In 2005, her family immigrated to Minnesota, and eventually to somewhere else (undisclosed) in order to provide a better life for Jennifer and her little brother, for which she is very grateful.</p>
 
-				<h3>Maria Mancheno</h3>
-					<p>Maria Mancheno is from Quito, Equador, and is 21 years of age. She was born and raised in Quito, as well as attended school there in the "American School of Quito", where she acquired an interest in the natural sciences. She attributes this to her preparedness for the rigor that the University of Rochester has to offer.</p>
+			<article class= "article">
+				<div class="box a">
+						<img src = "images/alex.jpg", alt="Alex" class="image">
+									<a href="/alex.jpg">
+											<!-- <div class="text">Alex's interests</div> -->
+									</a>
+				</div>
+
+
+				<div class="box b">
+											<!-- Section could be the menu in the first page for each
+											student & the content on other pages
+												-->
+							<h3>Alex Chase</h3>
+							<p>Alex Chase was evidently born at a young age (who knew??) and raised by wolves until he finally made the realization that fighting for scraps of meat amongst wolf pups was not only not the right life path for him, but was in fact gen-ed credit in high school terms! Alex graduated from high school with honors and proceeded to go to the University of Rochester, just like the rest of us.</p>
+
+				</div>
+
+
+
+
+				<div class="box c">
+						<img src = "images/jennifer.jpg", alt="Jennifer" class="image">
+							<div class = "middle">
+									<a href="/alex.jpg">
+											<!-- <div class="text">Jennifer's interests</div> -->
+									</a>
+							</div>
+				</div>
+
+				<div class="box d">
+					<h3>Jennifer Ngene</h3>
+						<p>Jennifer Ngene was born in The Gambia in 1999 and was raised between there and Nigeria for the first few years of life. In 2005, her family immigrated to Minnesota, and eventually to somewhere else (undisclosed) in order to provide a better life for Jennifer and her little brother, for which she is very grateful.</p>
+
+				</div>
+
+				<div class="box e">
+						<img src = "images/maria.jpg", alt="Jennifer" class="image">
+							<div class = "middle">
+									<a href="/maria.jpg">
+											<!-- <div class="text">Jennifer's interests</div> -->
+									</a>
+							</div>
+				</div>
+
+				<div class="box f">
+					<h3>Maria Mancheno</h3>
+						<p>Maria Mancheno is from Quito, Equador, and is 21 years of age. She was born and raised in Quito, as well as attended school there in the "American School of Quito", where she acquired an interest in the natural sciences. She attributes this to her preparedness for the rigor that the University of Rochester has to offer.</p>
+					</div>
+
+		</article>
 
 
 		</main>
+
+		<nav class = "menu-inner">
+		<ul>
+			<li>Alex Chase</li>
+			<li>Jennifer Ngene</li>
+			<li>Maria Mancheno</li>
+		</ul>
+	</nav>
 
 		<footer>CSC174: Advanced Front-End Web Design and Development</footer>
 
