aa145941f99a852830c3a594c0933350424c2bcd
Baz D <deweybazirake@gmail.com>
2019-09-27 16:50:56 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,25 @@
 <!doctype html>
-
-	<html land="en">
+<html land="en">
 
 	<head>
 		<meta charset="utf-8">
-		<title>Home</title>
+		<title>Team Havana | Assignment 03: IA</title>
 		<link rel="stylesheet" href ="css/styles.css">
 	</head>
 
-	<body>
+	<body class = "container">
 
 		<header>
-
-			<h1>The Persons of Interest: DMS Majors</h1>
-			<ul>
-				<li>Alex Chase</li>
-				<li>Jennifer Ngene</li>
-				<li>Maria Mancheno</li>
-			</ul>
-
+			<h1 class="homeh1" style = "font-size: 1.2em; left: -65px; font-size: 42px;" > We are DMS</h1>
 		</header>
 
-		<nav>
+		<section class="section">
+			<h2>Topics of Similarity</h2>
+			<p>The aforementioned students are all declared Digital Media Studies Majors, and all go about discussing their early lives, interests, and why it is that they are a DMS major.</p>
+		</section>
+
+		<!-- Possibility of adding aside -->
+		<nav class="menu">
 
 			<ul>
 				<li><a href="index.php">Home</a></li>
@@ -34,10 +32,98 @@
 
 		<main>
 
-			<h2>Topics of Similarity</h2>
-			<p>The aforementioned students are all declared Digital Media Studies Majors, and all go about discussing their early lives, interests, and why it is that they are a DMS major.</p>
+			<!--  <a href="alex.php">
+				<h1 class="homeh1" style = "font-size: 1.2em; left: -65px; font-size: 42px;" > Welcome to our page!</h1>
+			</a> -->
+
+
+			<h2>Home</h2>
+
+
+			<article class= "article">
+				<div class="box a">
+						<img src = "images/alex.jpg", alt="Alex" class="image">
+									<a href="/alex.jpg">
+											<!-- <div class="text">Alex's interests</div> -->
+									</a>
+				</div>
+
+
+				<div class="box b">
+											<!-- Section could be the menu in the first page for each
+											student & the content on other pages
+												-->
+						<nav class="article-sub">
+
+							<ul>
+								<li><a href="#">Alex's earlier life</a></li>
+								<li><a href="#">Alex's interests</a></li>
+								<li><a href="#">Alex in DMS</a></li>
+							</ul>
+
+						</nav>
+				</div>
+
+
+
+
+				<div class="box c">
+						<img src = "images/jennifer.jpg", alt="Jennifer" class="image">
+							<div class = "middle">
+									<a href="/alex.jpg">
+											<!-- <div class="text">Jennifer's interests</div> -->
+									</a>
+							</div>
+				</div>
+
+				<div class="box d">
+					<nav class="#">
+
+						<ul>
+							<li><a href="#">Home</a></li>
+							<li><a href="#">Jennifer's Earlier Life</a></li>
+							<li><a href="#">Jennifer's Interests</a></li>
+							<li><a href="#">Jennifer in DMS</a></li>
+						</ul>
+
+					</nav>
+				</div>
+
+				<div class="box e">
+						<img src = "images/maria.jpg", alt="Jennifer" class="image">
+							<div class = "middle">
+									<a href="/maria.jpg">
+											<!-- <div class="text">Jennifer's interests</div> -->
+									</a>
+							</div>
+				</div>
+
+				<div class="box f">
+					<nav class="#">
+						<ul>
+							<li><a href="#">Home</a></li>
+							<li><a href="#">Maria's Earlier Life</a></li>
+							<li><a href="#">Maria's Interests</a></li>
+							<li><a href="#">Maria in DMS</a></li>
+						</ul>
+					</nav>
+
+
+
+
+
+		</article>
+
 
 		</main>
+
+		<nav class = "menu-inner">
+		<ul>
+			<li>Alex Chase</li>
+			<li>Jennifer Ngene</li>
+			<li>Maria Mancheno</li>
+		</ul>
+	</nav>
 
 		<footer>CSC174: Advanced Front-End Web Design and Development</footer>
 
