3abff7849fdc21920eeeac18a53c185554a35b53
Baz D <deweybazirake@gmail.com>
2019-09-27 16:51:02 -0400

    Update why-dms.php

--- why-dms.php
+++ why-dms.php
@@ -1,27 +1,25 @@
 <!doctype html>
-
-	<html land="en">
+<html land="en">
 
 	<head>
 		<meta charset="utf-8">
-		<title>Home</title>
+		<title>Team Havana | Assignment 03: IA</title>
 		<link rel="stylesheet" href ="css/styles.css">
 	</head>
 
-	<body>
+	<body class = "container">
 
 		<header>
-
-			<h1>The Persons of Interest: DMS Majors</h1>
-			<ul>
-				<li>Alex Chase</li>
-				<li>Jennifer Ngene</li>
-				<li>Maria Mancheno</li>
-			</ul>
-
+			<h1 class="homeh1" style = "font-size: 1.2em; left: -65px; font-size: 42px;" > We are DMS</h1>
 		</header>
 
-		<nav>
+		<section class="section">
+			<h2>Topics of Similarity</h2>
+			<p>The aforementioned students are all declared Digital Media Studies Majors, and all go about discussing their early lives, interests, and why it is that they are a DMS major.</p>
+		</section>
+
+		<!-- Possibility of adding aside -->
+		<nav class="menu">
 
 			<ul>
 				<li><a href="index.php">Home</a></li>
@@ -34,17 +32,75 @@
 
 		<main>
 
-			<h2>Why DMS?</h2>
-				<h3>Alex Chase</h3>
-					<p>Alex makes the claim that it is due to his bountiful "drive to create", as well as the drive to solve the world's up and coming problems. Furthermore, whatever Alex winds up doing with the rest of his life, he hopes that it's something that he at the very least enjoys doing, and my be considered "neat".</p>
+			<!--  <a href="alex.php">
+				<h1 class="homeh1" style = "font-size: 1.2em; left: -65px; font-size: 42px;" > Welcome to our page!</h1>
+			</a> -->
 
-				<h3>Jennifer Ngene</h3>
-					<p>Jennifer saw the light in terms of web design, and from there realized the potential that dms could have for her creativity. Furthermore, it can be used as an outlet for her skills and a way for her to learn more about the field.</p>
 
-				<h3>Maria Mancheno</h3>
+			<h2>Why DMS</h2>
+
+
+			<article class= "article">
+				<div class="box a">
+						<img src = "images/alex.jpg", alt="Alex" class="image">
+									<a href="/alex.jpg">
+											<!-- <div class="text">Alex's interests</div> -->
+									</a>
+				</div>
+
+
+				<div class="box b">
+											<!-- Section could be the menu in the first page for each
+											student & the content on other pages
+												-->
+					<h3>Alex Chase</h3>
+							<p>Alex makes the claim that it is due to his bountiful "drive to create", as well as the drive to solve the world's up and coming problems. Furthermore, whatever Alex winds up doing with the rest of his life, he hopes that it's something that he at the very least enjoys doing, and my be considered "neat".</p>
+
+				</div>
+
+
+
+
+				<div class="box c">
+						<img src = "images/jennifer.jpg", alt="Jennifer" class="image">
+							<div class = "middle">
+									<a href="/alex.jpg">
+											<!-- <div class="text">Jennifer's interests</div> -->
+									</a>
+							</div>
+				</div>
+
+				<div class="box d">
+					<h3>Jennifer Ngene</h3>
+						<p>Jennifer saw the light in terms of web design, and from there realized the potential that dms could have for her creativity. Furthermore, it can be used as an outlet for her skills and a way for her to learn more about the field.</p>
+
+				</div>
+
+				<div class="box e">
+						<img src = "images/maria.jpg", alt="Jennifer" class="image">
+							<div class = "middle">
+									<a href="/maria.jpg">
+											<!-- <div class="text">Jennifer's interests</div> -->
+									</a>
+							</div>
+				</div>
+
+				<div class="box f">
+					<h3>Maria Mancheno</h3>
 					<p>Maria's appeal to DMS came through her interest in website design, as it seemed to be a potential avenue for jobs in the future. Where mathematics was something that made more sense to her, it was not necessarily something that she wished to pursue as a career in the future, thus she leaned more towards DMS as a major, and Mathematics as a minor.</p>
 
+		</article>
+
+
 		</main>
+
+		<nav class = "menu-inner">
+		<ul>
+			<li>Alex Chase</li>
+			<li>Jennifer Ngene</li>
+			<li>Maria Mancheno</li>
+		</ul>
+	</nav>
 
 		<footer>CSC174: Advanced Front-End Web Design and Development</footer>
 
