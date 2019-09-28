93495e154f369804951c6938c5a8811f184ab144
Baz D <deweybazirake@gmail.com>
2019-09-27 22:32:04 -0400

    Update interests.php

--- interests.php
+++ interests.php
@@ -30,6 +30,11 @@
 
 		</nav>
 
+		<section>
+			<h2>Interests</h2>
+		</section>
+
+
 		<main>
 
 			<!--  <a href="alex.php">
@@ -37,61 +42,52 @@
 			</a> -->
 
 
-			<h2>Interests</h2>
 
 
-			<article class= "article">
-				<div class="box a">
+				<section class="box a">
 						<img src = "images/alex.jpg", alt="Alex" class="image">
 									<a href="/alex.jpg">
-											<!-- <div class="text">Alex's interests</div> -->
+											<!-- <section class="text">Alex's interests</section> -->
 									</a>
-				</div>
+				</section>
 
 
-				<div class="box b">
+				<section class="box b">
 											<!-- Section could be the menu in the first page for each
 											student & the content on other pages
 												-->
 						<h3>Alex Chase</h3>
 						<p>Alex has expressed great interest in music and the arts as a whole. He is a guitar player, favoring rock and blues, and has found himself joining NJR on campus for a few years. His aptitude for fraphic design allowed for him to obtain an internship with his state senator in the field of graphic design. On the side he trains in Capoeira, an Afro-Brazilian martial art.</p>
 
-				</div>
+				</section>
 
 
 
 
-				<div class="box c">
+				<section class="box c">
 						<img src = "images/jennifer.jpg", alt="Jennifer" class="image">
-							<div class = "middle">
 									<a href="/alex.jpg">
-											<!-- <div class="text">Jennifer's interests</div> -->
+											<!-- <section class="text">Jennifer's interests</section> -->
 									</a>
-							</div>
-				</div>
+				</section>
 
-				<div class="box d">
+				<section class="box d">
 					<h3>Jennifer Ngene</h3>
 						<p>Jennifer developed her interest in the field of web design by looking at a multitude of bloggers, which spurred in tenth grade. Upon experimenting with the in-browser editor, she further developed her interst and skills in that which would become her topic of interst for the future. She would later work with fellow IB students to develop a website using WIX.</p>
 
-				</div>
+				</section>
 
-				<div class="box e">
+				<section class="box e">
 						<img src = "images/maria.jpg", alt="Jennifer" class="image">
-							<div class = "middle">
 									<a href="/maria.jpg">
-											<!-- <div class="text">Jennifer's interests</div> -->
+											<!-- <section class="text">Jennifer's interests</section> -->
 									</a>
-							</div>
-				</div>
+				</section>
 
-				<div class="box f">
+				<section class="box f">
 					<h3>Maria Mancheno</h3>
 						<p>Maria Mancheno is from Quito, Equador, and is 21 years of age. She was born and raised in Quito, as well as attended school there in the "American School of Quito", where she acquired an interest in the natural sciences. She attributes this to her preparedness for the rigor that the University of Rochester has to offer.</p>
-					</div>
-
-		</article>
-
+					</section>
 
 		</main>
 
