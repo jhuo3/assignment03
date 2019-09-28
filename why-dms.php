2241135046a2643fea1acf3a7b17084c59050214
Baz D <deweybazirake@gmail.com>
2019-09-27 22:32:07 -0400

    Update why-dms.php

--- why-dms.php
+++ why-dms.php
@@ -18,6 +18,8 @@
 			<p>The aforementioned students are all declared Digital Media Studies Majors, and all go about discussing their early lives, interests, and why it is that they are a DMS major.</p>
 		</section>
 
+
+
 		<!-- Possibility of adding aside -->
 		<nav class="menu">
 
@@ -30,67 +32,62 @@
 
 		</nav>
 
+
+		<section>
+			<h2>Why DMS</h2>
+		</section>
+
+
 		<main>
 
 			<!--  <a href="alex.php">
 				<h1 class="homeh1" style = "font-size: 1.2em; left: -65px; font-size: 42px;" > Welcome to our page!</h1>
 			</a> -->
-
-
-			<h2>Why DMS</h2>
-
-
-			<article class= "article">
-				<div class="box a">
+				<section>
 						<img src = "images/alex.jpg", alt="Alex" class="image">
 									<a href="/alex.jpg">
 											<!-- <div class="text">Alex's interests</div> -->
 									</a>
-				</div>
+				</section>
 
 
-				<div class="box b">
+				<section>
 											<!-- Section could be the menu in the first page for each
 											student & the content on other pages
 												-->
 					<h3>Alex Chase</h3>
 							<p>Alex makes the claim that it is due to his bountiful "drive to create", as well as the drive to solve the world's up and coming problems. Furthermore, whatever Alex winds up doing with the rest of his life, he hopes that it's something that he at the very least enjoys doing, and my be considered "neat".</p>
 
-				</div>
+				</section>
 
 
 
 
-				<div class="box c">
+				<section>
 						<img src = "images/jennifer.jpg", alt="Jennifer" class="image">
-							<div class = "middle">
 									<a href="/alex.jpg">
 											<!-- <div class="text">Jennifer's interests</div> -->
 									</a>
-							</div>
-				</div>
+				</section>
 
-				<div class="box d">
+				<section>
 					<h3>Jennifer Ngene</h3>
 						<p>Jennifer saw the light in terms of web design, and from there realized the potential that dms could have for her creativity. Furthermore, it can be used as an outlet for her skills and a way for her to learn more about the field.</p>
 
-				</div>
+				</section>
 
-				<div class="box e">
+				<section class="box e">
 						<img src = "images/maria.jpg", alt="Jennifer" class="image">
-							<div class = "middle">
 									<a href="/maria.jpg">
-											<!-- <div class="text">Jennifer's interests</div> -->
 									</a>
 							</div>
-				</div>
+				</section>
 
-				<div class="box f">
+				<section class="box f">
 					<h3>Maria Mancheno</h3>
 					<p>Maria's appeal to DMS came through her interest in website design, as it seemed to be a potential avenue for jobs in the future. Where mathematics was something that made more sense to her, it was not necessarily something that she wished to pursue as a career in the future, thus she leaned more towards DMS as a major, and Mathematics as a minor.</p>
 
-		</article>
-
+				</section>
 
 		</main>
 
