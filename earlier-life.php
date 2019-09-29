41bada4d57b848fb0063c2df34ba5df026c765fb
Baz D <deweybazirake@gmail.com>
2019-09-29 16:32:43 -0400

    Update earlier-life.php

--- earlier-life.php
+++ earlier-life.php
@@ -13,28 +13,29 @@
 			<h1 class="homeh1" style = "font-size: 1.2em; left: -65px; font-size: 42px;" > We are DMS</h1>
 		</header>
 
-		<section class="section">
-			<h2>Topics of Similarity</h2>
-			<p>The aforementioned students are all declared Digital Media Studies Majors, and all go about discussing their early lives, interests, and why it is that they are a DMS major.</p>
-		</section>
 
-		<!-- Possibility of adding aside -->
-		<nav class="menu">
+				<section class="section">
+					<h2>Topics of Similarity</h2>
+					<p>The aforementioned students are all declared Digital Media Studies Majors, and all go about discussing their early lives, interests, and why it is that they are a DMS major.</p>
+				</section>
 
-			<ul>
-				<li><a href="index.php">Home</a></li>
-				<li><a href="earlier-life.php">Earlier Life</a></li>
-				<li><a href="interests.php">Interests</a></li>
-				<li><a href="why-dms.php">Why DMS</a></li>
-			</ul>
+				<nav class="menu-inner">
 
-		</nav>
+					<ul>
+						<li><a href="index.php">Home</a></li>
+						<li><a href="earlier-life.php">Earlier Life</a></li>
+						<li><a href="interests.php">Interests</a></li>
+						<li><a href="why-dms.php">Why DMS</a></li>
+					</ul>
+
+				</nav>
+
+				<section>
+						<h2>Earlier life</h2>
+				</section>
 
 
-		<section>
-			<h2>Early Life</h2>
-		</section>
-
+				<!-- Possibility of adding aside -->
 
 
 
@@ -45,7 +46,7 @@
 			</a> -->
 
 
-				<div class="box a">
+				<div class="box a" id="alex-life">
 						<img src = "images/alex.jpg", alt="Alex" class="image">
 									<a href="/alex.jpg">
 											<!-- <div class="text">Alex's interests</div> -->
@@ -65,28 +66,23 @@
 
 
 
-				<div class="box c">
+				<div class="box c" id = "jennifer-life">
 						<img src = "images/jennifer.jpg", alt="Jennifer" class="image">
-							<div class = "middle">
 									<a href="/alex.jpg">
 											<!-- <div class="text">Jennifer's interests</div> -->
 									</a>
-							</div>
 				</div>
 
 				<div class="box d">
 					<h3>Jennifer Ngene</h3>
 						<p>Jennifer Ngene was born in The Gambia in 1999 and was raised between there and Nigeria for the first few years of life. In 2005, her family immigrated to Minnesota, and eventually to somewhere else (undisclosed) in order to provide a better life for Jennifer and her little brother, for which she is very grateful.</p>
-
 				</div>
 
-				<div class="box e">
+				<div class="box e" id="maria-life">
 						<img src = "images/maria.jpg", alt="Jennifer" class="image">
-							<div class = "middle">
 									<a href="/maria.jpg">
 											<!-- <div class="text">Jennifer's interests</div> -->
 									</a>
-							</div>
 				</div>
 
 				<div class="box f">
@@ -95,14 +91,6 @@
 					</div>
 
 		</main>
-
-		<nav class = "menu-inner">
-		<ul>
-			<li>Alex Chase</li>
-			<li>Jennifer Ngene</li>
-			<li>Maria Mancheno</li>
-		</ul>
-	</nav>
 
 		<footer>CSC174: Advanced Front-End Web Design and Development</footer>
 
