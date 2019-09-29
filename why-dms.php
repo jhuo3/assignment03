e3fbfa5c0ee1eb1651ff5e14e411b9e4a349fb55
Baz D <deweybazirake@gmail.com>
2019-09-29 16:32:52 -0400

    Update why-dms.php

--- why-dms.php
+++ why-dms.php
@@ -13,29 +13,28 @@
 			<h1 class="homeh1" style = "font-size: 1.2em; left: -65px; font-size: 42px;" > We are DMS</h1>
 		</header>
 
-		<section class="section">
-			<h2>Topics of Similarity</h2>
-			<p>The aforementioned students are all declared Digital Media Studies Majors, and all go about discussing their early lives, interests, and why it is that they are a DMS major.</p>
-		</section>
+				<section class="section">
+					<h2>Topics of Similarity</h2>
+					<p>The aforementioned students are all declared Digital Media Studies Majors, and all go about discussing their early lives, interests, and why it is that they are a DMS major.</p>
+				</section>
+
+				<nav class="menu-inner">
+
+					<ul>
+						<li><a href="index.php">Home</a></li>
+						<li><a href="earlier-life.php">Earlier Life</a></li>
+						<li><a href="interests.php">Interests</a></li>
+						<li><a href="why-dms.php">Why DMS</a></li>
+					</ul>
+
+				</nav>
+
+				<section>
+					<h2>Why DMS?</h2>
+				</section>
 
 
-
-		<!-- Possibility of adding aside -->
-		<nav class="menu">
-
-			<ul>
-				<li><a href="index.php">Home</a></li>
-				<li><a href="earlier-life.php">Earlier Life</a></li>
-				<li><a href="interests.php">Interests</a></li>
-				<li><a href="why-dms.php">Why DMS</a></li>
-			</ul>
-
-		</nav>
-
-
-		<section>
-			<h2>Why DMS</h2>
-		</section>
+				<!-- Possibility of adding aside -->
 
 
 		<main>
@@ -43,7 +42,7 @@
 			<!--  <a href="alex.php">
 				<h1 class="homeh1" style = "font-size: 1.2em; left: -65px; font-size: 42px;" > Welcome to our page!</h1>
 			</a> -->
-				<section>
+				<section class="box a" id ="alex-dms">
 						<img src = "images/alex.jpg", alt="Alex" class="image">
 									<a href="/alex.jpg">
 											<!-- <div class="text">Alex's interests</div> -->
@@ -51,7 +50,7 @@
 				</section>
 
 
-				<section>
+				<section class="box b">
 											<!-- Section could be the menu in the first page for each
 											student & the content on other pages
 												-->
@@ -63,20 +62,20 @@
 
 
 
-				<section>
+				<section class="box c" id="jennifer-dms">
 						<img src = "images/jennifer.jpg", alt="Jennifer" class="image">
 									<a href="/alex.jpg">
 											<!-- <div class="text">Jennifer's interests</div> -->
 									</a>
 				</section>
 
-				<section>
+				<section class="box d">
 					<h3>Jennifer Ngene</h3>
 						<p>Jennifer saw the light in terms of web design, and from there realized the potential that dms could have for her creativity. Furthermore, it can be used as an outlet for her skills and a way for her to learn more about the field.</p>
 
 				</section>
 
-				<section class="box e">
+				<section class="box e" id="maria-dms">
 						<img src = "images/maria.jpg", alt="Jennifer" class="image">
 									<a href="/maria.jpg">
 									</a>
@@ -91,13 +90,6 @@
 
 		</main>
 
-		<nav class = "menu-inner">
-		<ul>
-			<li>Alex Chase</li>
-			<li>Jennifer Ngene</li>
-			<li>Maria Mancheno</li>
-		</ul>
-	</nav>
 
 		<footer>CSC174: Advanced Front-End Web Design and Development</footer>
 
